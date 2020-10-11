cf268a143eed0930d5cede59e6743dc697535bf8
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-10-11 21:46:47 +0300

    11102020

--- src/js/components/popup.js
+++ src/js/components/popup.js
@@ -10,12 +10,12 @@ $('.btn--popup').on('click', function (e) {
   });
   
   
-  $document = $(document);
-  $document.mouseup(function (e){ // событие клика по веб-документу
-    var div = $(".popup-wrapper"); // тут указываем элемент
-    if (!div.is(e.target) // если клик был не по нашему блоку
-        && div.has(e.target).length === 0) { // и не по его дочерним элементам
-          $('.popup-overlay').hide();
-          $('body').css('overflow', 'initial');
-    }
-  });
+  // $document = $(document);
+  // $document.mouseup(function (e){ // событие клика по веб-документу
+  //   var div = $(".popup-wrapper"); // тут указываем элемент
+  //   if (!div.is(e.target) // если клик был не по нашему блоку
+  //       && div.has(e.target).length === 0) { // и не по его дочерним элементам
+  //         $('.popup-overlay').hide();
+  //         $('body').css('overflow', 'initial');
+  //   }
+  // });
--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,21 @@
-//= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
 //= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
-//= ../../node_modules/imagelightbox/dist/imagelightbox.min.js
 
 $(function(){
 
-   //= components/header.js
-   //= components/burger.js
-   //= components/readmore.js
-   //= components/slider.js
-   //= components/map.js
-   //= components/popup.js
+  //= components/burger.js
+    //= components/popup.js
+    //= components/slider.js
+    //= components/header.js
+    //= components/readmore.js
+    //= components/map.js
 
    if ($(window).width() < 600) {
       $(".owl-stage-outer").readmore({
         maxHeight: 50,
         moreLink:
-          '<a href="#" style="margin:10px 5px;color:#fff">Развернуть текст</a>',
-        lessLink: '<a href="#" style="margin:10px 5px;color:#fff" >Свернуть текст</a>',
+          '<a href="#" style="margin:10px 5px;color:#fff">>></a>',
+        lessLink: '<a href="#" style="margin:10px 5px;color:#fff" ><<</a>',
       });
     }
 
